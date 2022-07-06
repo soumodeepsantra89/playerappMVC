2c3d5b0e414778ad5d07c764fb3111b8095f00ce
unknown <soumodeepsantra89@gmail.com>
2022-07-06 17:31:16 +0530

    newcommit

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            List<player> listplayers = new List<player>
+            {
+                new player { id = 1, name ="Virat", average =56.23},
+                new player { id = 2, name ="Dhoni",average =33.60},
+                new player{ id = 3, name ="Rohit",average=55.22}
+            };
+            return View(listplayers);
+        }
+    }
+}
---
+++ Models/player.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class player
+    {
+        public int id { get; set; }
+        public string name { get; set; }    
+        public double average { get; set; }
+
+    }
+}
